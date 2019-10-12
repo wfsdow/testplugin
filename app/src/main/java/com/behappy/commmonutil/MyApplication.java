7d18dace5cd3376c839d5d9cd38807c3d405c8ff
ningwei <wei.ning@guuidea.com>
2019-10-12 11:38:21 +0800

    完成有自更新功能的apk

--- app/src/main/java/com/behappy/commmonutil/MyApplication.java
+++ app/src/main/java/com/behappy/commmonutil/MyApplication.java
@@ -3,12 +3,12 @@ package com.behappy.commmonutil;
 import android.app.Application;
 import android.content.Context;
 
-import com.didi.virtualapk.PluginManager;
+//import com.didi.virtualapk.PluginManager;
 
 public class MyApplication extends Application {
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
-        PluginManager.getInstance(base).init();
+//        PluginManager.getInstance(base).init();
     }
 }
--- app/src/main/java/com/behappy/commmonutil/Utils.java
+++ app/src/main/java/com/behappy/commmonutil/Utils.java
@@ -3,21 +3,21 @@ package com.behappy.commmonutil;
 import android.content.Context;
 import android.content.Intent;
 
-import com.didi.virtualapk.PluginManager;
+//import com.didi.virtualapk.PluginManager;
 
 import java.io.File;
 
 public class Utils {
-    private void loadPlg(Context context, String filePath, String packageName, String className) {
-        File plugin = new File(filePath);
-        try {
-            PluginManager.getInstance(context).loadPlugin(plugin);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        Intent intent = new Intent();
-        intent.setClassName(packageName, className);
-        context.startActivity(intent);
-    }
+//    private void loadPlg(Context context, String filePath, String packageName, String className) {
+//        File plugin = new File(filePath);
+//        try {
+//            PluginManager.getInstance(context).loadPlugin(plugin);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//        Intent intent = new Intent();
+//        intent.setClassName(packageName, className);
+//        context.startActivity(intent);
+//    }
 }
